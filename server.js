7a0634101f3622608baf4dcbbc38b0237cd09c9b
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2016-03-20 13:42:40 -0400

    adds api

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-import * as todo from "./todo";
+import express from "express";
+import bodyParser from "body-parser";
 import R from "ramda";
+import * as todo from "./todo";
 
-let events = [
-  {
-    type: "taskAdded",
-    data: {
-      id: "665",
-      description: "dishes"
-    }
-  },
-  {
-    type: "taskCompleted",
-    data: {
-      id: "665"
-    }
-  }
-];
+let eventStore = [];
 
-const command = {
-  type: "addTask",
-  data: {
-    id: "666", 
-    description: "homework"
-  }
-};
+const app = express();
+app.use(bodyParser.json());
 
-const state = R.reduce(R.flip(todo.apply), [], events);
+app.get("/events", (req, res) => {
+  res.send(eventStore);
+});
 
-events = R.append(todo.handle(command, state), events);
+app.get("/state", (req, res) => {
+  res.send(R.reduce(R.flip(todo.apply), [], eventStore));
+});
 
-console.log("state:", state);
-console.log("events:", events);
+app.post("/commands", (req, res) => {
+  const state = R.reduce(R.flip(todo.apply), [], eventStore);
+  const events = todo.handle(req.body, state);
+  eventStore = R.concat(events, eventStore);
+  res.send(events);
+});
+
+const server = app.listen(3000, () => {
+  let port = server.address().port;
+  console.log("server listening at http://localhost:%s", port);
+});
 
